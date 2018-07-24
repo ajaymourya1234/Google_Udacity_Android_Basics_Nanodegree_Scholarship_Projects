6f19d3da41cc3cb42bf1ff4c1bb2367b91c21005
ajaymourya1234 <ajaymourya8553@gmail.com>
2018-07-24 17:56:58 +0530

    completed the music app

--- MusicApp/app/src/main/java/com/example/ajay/musicapp/MainActivity.java
+++ MusicApp/app/src/main/java/com/example/ajay/musicapp/MainActivity.java
@@ -27,59 +27,37 @@ public class MainActivity extends AppCompatActivity {
 
         // Create a ArrayList of Song objects
         ArrayList<Song> songs = new ArrayList<Song>();
-        songs.add(new Song(R.drawable.audio_jack, "Tap to play song 1", "Artist 1"));
-        songs.add(new Song(R.drawable.audio_jack, "Tap to play song 2", "Artist 2"));
-        songs.add(new Song(R.drawable.audio_jack, "Tap to play song 3", "Artist 3"));
-        songs.add(new Song(R.drawable.audio_jack, "Tap to play song 4", "Artist 4"));
-        songs.add(new Song(R.drawable.audio_jack, "Tap to play song 5", "Artist 5"));
-        songs.add(new Song(R.drawable.audio_jack, "Tap to play song 6", "Artist 6"));
-        songs.add(new Song(R.drawable.audio_jack, "Tap to play song 7", "Artist 7"));
-        songs.add(new Song(R.drawable.audio_jack, "Tap to play song 8", "Artist 8"));
-        songs.add(new Song(R.drawable.audio_jack, "Tap to play song 1", "Artist 9"));
+        songs.add(new Song(R.drawable.audio_jack, getString(R.string.song_name_1), getString(R.string.artist_1)));
+        songs.add(new Song(R.drawable.audio_jack, getString(R.string.song_name_2), getString(R.string.artist_2)));
+        songs.add(new Song(R.drawable.audio_jack, getString(R.string.song_name_3), getString(R.string.artist_3)));
+        songs.add(new Song(R.drawable.audio_jack, getString(R.string.song_name_4), getString(R.string.artist_4)));
+        songs.add(new Song(R.drawable.audio_jack, getString(R.string.song_name_5), getString(R.string.artist_5)));
+        songs.add(new Song(R.drawable.audio_jack, getString(R.string.song_name_6), getString(R.string.artist_6)));
+        songs.add(new Song(R.drawable.audio_jack, getString(R.string.song_name_7), getString(R.string.artist_7)));
+        songs.add(new Song(R.drawable.audio_jack, getString(R.string.song_name_8), getString(R.string.artist_8)));
+        songs.add(new Song(R.drawable.audio_jack, getString(R.string.song_name_9), getString(R.string.artist_9)));
+        songs.add(new Song(R.drawable.audio_jack, getString(R.string.song_name_10), getString(R.string.artist_10)));
 
 
-        SongAdapter adapter = new SongAdapter(this, songs);
+
+        final SongAdapter adapter = new SongAdapter(this, songs);
 
         // Get a reference to the ListView, and attach the adapter to the listView.
         ListView listView = (ListView) findViewById(R.id.listview_flavor);
         listView.setAdapter(adapter);
 
 
-        AdapterView.OnItemClickListener itemClickListener = new AdapterView.OnItemClickListener() {
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-
-                // Getting the Container Layout of the ListView
-                linearLayoutParent = (LinearLayout) view;
-
-                // Getting the inner Linear Layout
-                image = (ImageView) linearLayoutParent.getChildAt(0);
-
-                // Getting the inner Linear Layout
-                linearLayoutChild = (LinearLayout ) linearLayoutParent.getChildAt(1);
-
-                // Getting the Country TextView
-                songName = (TextView) linearLayoutChild.getChildAt(0);
-
-                // Getting the Country TextView
-                songArtist = (TextView) linearLayoutChild.getChildAt(1);
-
-                Toast.makeText(getBaseContext(), songArtist.getText().toString(), Toast.LENGTH_SHORT).show();
-
-
-
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Song item = (Song) adapter.getItem(position);
+                Intent nowPlaying = new Intent(getApplicationContext(), SongPlaying.class);
+                nowPlaying.putExtra("KEY_SONG", item.getSongName());
+                //nowPlaying.putExtra(KEY_ALBUM, item.getAlbum());
+                nowPlaying.putExtra("KEY_ARTIST", item.getSongArtist());
+                startActivity(nowPlaying);
             }
-        };
-        // Setting the item click listener for the listview
-        listView.setOnItemClickListener(itemClickListener);
-
-        /* Intent _intent = new Intent(this, SongPlaying.class);
-        Bitmap _bitmap; // your bitmap
-        ByteArrayOutputStream _bs = new ByteArrayOutputStream();
-        _bitmap.compress(Bitmap.CompressFormat.PNG, 50, _bs);
-        _intent.putExtra("byteArray", _bs.toByteArray());
-        startActivity(i);*/
-
+        });
     }
 
 }
--- MusicApp/app/src/main/java/com/example/ajay/musicapp/SongPlaying.java
+++ MusicApp/app/src/main/java/com/example/ajay/musicapp/SongPlaying.java
@@ -1,7 +1,10 @@
 package com.example.ajay.musicapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class SongPlaying extends AppCompatActivity {
@@ -11,9 +14,41 @@ public class SongPlaying extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_song_playing);
 
-        String s = getIntent().getStringExtra("user");
+        String playingSong = "";
+        String playingArtist = "";
+        String playingAlbum = "";
 
-        TextView songName = (TextView) findViewById(R.id.song_name1);
-        songName.setText(s);
+        Intent intent = getIntent();
+        if (null != intent) {
+            playingSong = intent.getStringExtra("KEY_SONG");
+            playingArtist = intent.getStringExtra("KEY_ARTIST");
+            playingAlbum = intent.getStringExtra("KEY_ALBUM");
+
+            TextView playingSongText = (TextView) findViewById(R.id.now_playing_song);
+            playingSongText.setText(playingSong);
+
+            TextView songArtistText = (TextView) findViewById(R.id.playing_song_artist);
+            songArtistText.setText(playingArtist);
+
+            final ImageView playButton = (ImageView) findViewById(R.id.icon_play);
+
+            // Sets tag to switch ImageView between play and pause buttons when clicked.
+            // Reference: https://stackoverflow.com/questions/32420565/android-changing-imageview-between-two-images-on-button-click-and-reclick
+            playButton.setTag(1);
+            playButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (playButton.getTag().equals(1)) {
+                        playButton.setImageResource(R.drawable.ic_pause_black_48dp);
+                        playButton.setTag(2);
+                    } else {
+                        playButton.setImageResource(R.drawable.ic_play_arrow_black_48dp);
+                        playButton.setTag(1);
+                    }
+
+                }
+            });
+            // End reference
+        }
     }
 }
